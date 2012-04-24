59d8ae28e0596d078f9bd73c082f8278ed91ccc7
conzi <tconzi@gmail.com>
2012-04-24 18:21:51 +0800

    update demo

--- demo.php
+++ demo.php
@@ -5,7 +5,7 @@ require_once('application/libraries/APN.php');
 
 $apn = new APN();
 
-$device_token = '6decb0f4d10c6dcfc416551073da108bd22c7d2b88965f5ac8a5114f2c754e5e1c1'; //设备token ,64位
+$device_token = '6decb0f4d10c6dcfc416551073da108bd22c7d2b88965f5ac8a5114f2c754e5e'; //设备token ,64位
 if($apn->push($device_token,'test',2) ) {
 
 	echo "Success! \n";
